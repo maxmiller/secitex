18bda0ef373721ac428835001f88c3134bbd1e4e
[3~ <clauber@debian-ifrn-ead.localdomain>
2018-08-21 17:41:57 +0000

    Correção da visualização da lista de trabalhos no admin

--- app/helpers/admin/trabalhos_helper.rb
+++ app/helpers/admin/trabalhos_helper.rb
@@ -1,2 +1,13 @@
 module Admin::TrabalhosHelper
+  def situacao(situacao)
+    if situacao == AvaliacaoTrabalho::SITUACOES[:aprovado]
+      return "<span class='label label-success'>Aprovado</span>"
+    elsif situacao == AvaliacaoTrabalho::SITUACOES[:reprovado]
+      return "<span class='label label-danger'>Reprovado</span>"
+    elsif situacao == AvaliacaoTrabalho::SITUACOES[:outra_linha]
+      return "<span class='label label-warning'>Outra linha</span>"
+    else
+      return "<span class='label label-default'>Pendente</span>"
+    end
+  end
 end
